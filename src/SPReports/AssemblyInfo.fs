f202e7aac36758221adcc922c617347771d2fc21
Ronnie Holm <mail@bugfree.dk>
2017-09-19 12:42:09 +0200

    Updated

--- src/SPReports/AssemblyInfo.fs
+++ src/SPReports/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Reflection
 [<assembly: AssemblyDescription("Generates reports of SharePoint Online tenant usage.")>]
 [<assembly: AssemblyProduct("SPReports")>]
 [<assembly: AssemblyCopyright("Copyright (c) 2016-2017 Ronnie Holm <mail@bugfree.dk>")>]
-[<assembly: AssemblyVersion("1.5.0")>]
-[<assembly: AssemblyFileVersion("1.5.0")>]
+[<assembly: AssemblyVersion("1.5.1")>]
+[<assembly: AssemblyFileVersion("1.5.1")>]
 
 do ()
